184971e7d46b44dd7b7b8c93403d4491a8c72429
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-01-06 23:31:49 -0300

    implementado pool de conexão

--- src/dev/rvz/StoreVirtualApplication.java
+++ src/dev/rvz/StoreVirtualApplication.java
@@ -1,5 +1,6 @@
 package dev.rvz;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import dev.rvz.configurations.ScreenConfig;
 import dev.rvz.connections.CreateConnectionFactory;
 import dev.rvz.core.MainMenu;
@@ -9,7 +10,13 @@ import java.sql.SQLException;
 public class StoreVirtualApplication {
 
     public static void main(String[] args) throws SQLException {
-        ScreenConfig screenConfig = new ScreenConfig(new CreateConnectionFactory().getConnection());
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mariadb://localhost:3306/loja_virtual");
+        comboPooledDataSource.setUser("loja_virtual");
+        comboPooledDataSource.setPassword("loja_virtual");
+
+        CreateConnectionFactory connectionFactory = new CreateConnectionFactory(comboPooledDataSource);
+        ScreenConfig screenConfig = new ScreenConfig(connectionFactory.getConnection());
         MainMenu mainMenu = new MainMenu(screenConfig.build());
         mainMenu.runningSystemMainMenu();
         screenConfig.getConnection().close();
--- src/dev/rvz/connections/CreateConnectionFactory.java
+++ src/dev/rvz/connections/CreateConnectionFactory.java
@@ -1,21 +1,24 @@
 package dev.rvz.connections;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class CreateConnectionFactory {
 
+    private final ComboPooledDataSource comboPooledDataSource;
     private Connection connection;
+    private DataSource dataSource;
+
+    public CreateConnectionFactory(ComboPooledDataSource comboPooledDataSource) {
+        this.comboPooledDataSource = comboPooledDataSource;
+        this.dataSource = this.comboPooledDataSource;
+    }
 
     public Connection getConnection() throws SQLException {
-        if (connection == null) {
-            connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/loja_virtual",
-                    "loja_virtual",
-                    "loja_virtual");
-            return connection;
-        }
-
+        this.connection = this.dataSource.getConnection();
         return connection;
     }
 }

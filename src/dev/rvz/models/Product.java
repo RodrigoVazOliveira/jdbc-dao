b50c9fa445c7b572247c199d9295762cdc139271
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-01-09 02:29:50 -0300

    Adicionar o campo categoria no produto

--- src/dev/rvz/models/Product.java
+++ src/dev/rvz/models/Product.java
@@ -4,11 +4,13 @@ public class Product {
     private final Integer id;
     private final String name;
     private final String description;
+    private final Integer category;
 
-    public Product(Integer id, String name, String description) {
+    public Product(Integer id, String name, String description, Integer category) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.category = category;
     }
 
     public Integer getId() {
@@ -23,12 +25,17 @@ public class Product {
         return description;
     }
 
+    public Integer getCategory() {
+        return category;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
+                ", category=" + category +
                 '}';
     }
 }
